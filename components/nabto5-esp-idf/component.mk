79a84c07b2088b3a5ba9b13ae10c25c38f1fdb24
Michael <mkm@nabto.com>
2019-11-14 18:56:02 +0100

    added embedded sdk as a submodule and prepared for new esp32 code

--- components/nabto5-esp-idf/component.mk
+++
@@ -1,7 +0,0 @@
-COMPONENT_ADD_INCLUDEDIRS := ../nabto-embedded-sdk/include ../nabto-embedded-sdk/src ../nabto-embedded-sdk/src/types/linux/ ../nabto-embedded-sdk/nabto-common-cpp/src/
-COMPONENT_SRCDIRS := \
-	./ \
-	../nabto-embedded-sdk/src/core \
-	../nabto-embedded-sdk/src/platform \
-	../nabto-embedded-sdk/src/api \
-	../nabto-embedded-sdk/nabto-common-cpp/src/streaming
